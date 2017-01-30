fca9870035e777e9afdee598d868f68a97afa5da
fvafrCU <dominik.cullmann@forst.bwl.de>
2017-01-30 20:27:27 +0100

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='contents',
-    version='0.1.0',
+    version='0.2.0',
     description='XXX',
     long_description=readme,
     author='Andreas Dominik Cullmann',
