dd0d594744ae670248ec05e8e5fb7398f354dc91
foobar <none@nowhere.no>
2017-01-31 17:48:48 +0100

    Uncommented changes: backing up

--- utils/run.py
+++ utils/run.py
@@ -3,3 +3,6 @@ import contents
 print(contents.get_toc(file_name = "contents/contents.py", 
                        comment_character = '#', 
                        magic_character = '%'))
+print(contents.contents(file_name = "contents/contents.py", 
+                       comment_character = '#', 
+                       magic_character = '%'))
